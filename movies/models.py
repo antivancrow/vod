21d0a57451385581a7807d8efaafb5306d3ac0a4
Ewelina Dmowska <e.dmowskaa@gmail.com>
2021-02-21 16:47:25 +0100

    fix plural for category

--- movies/models.py
+++ movies/models.py
@@ -19,6 +19,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
